b8f9db06e0bc6d18c5a478570f2496d502956a3b
TalhaZaheer1 <tzaheer72@gmail.com>
2025-01-15 18:32:14 +0500

    input file continue

--- libs/inputs.c
+++ libs/inputs.c
@@ -1,5 +1,7 @@
 #include "./inputs.h"
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void runPrompt() {
   char input[1024];
@@ -14,7 +16,22 @@ void runPrompt() {
 void runFile(char *path) {
   FILE *sourceFile = fopen(path, "r");
   if (sourceFile == NULL) {
-    perror("Invalid file path: %s");
+    perror("Invalid file path: %s.");
     exit(1);
   }
+  fseek(sourceFile, 0, SEEK_END);
+  long int filePos = ftell(sourceFile);
+  if (filePos == -1L) {
+    perror("Error file position.");
+    exit(1);
+  }
+  char sourceCode[filePos];
+  size_t chrsRead = fread(sourceCode, 1, filePos, sourceFile);
+  if (chrsRead < filePos) {
+    perror("Error reading file.");
+    exit(1);
+  }
+  run(sourceCode);
 }
+
+void run()
