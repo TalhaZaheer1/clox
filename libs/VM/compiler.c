bbd0abd90b9f293f30cf5dded6f06fc4e3ebc847
TalhaZaheer1 <tzaheer72@gmail.com>
2025-01-20 03:13:36 +0500

    will be conflicting

--- libs/VM/compiler.c
+++ libs/VM/compiler.c
@@ -17,6 +17,20 @@ typedef enum {
   PREC_PRIMARY
 } Precedence;
 
+typedef enum {
+  PREC_NONE,
+  PREC_ASSIGNMENT, // =
+  PREC_OR,         // or
+  PREC_AND,        // and
+  PREC_EQUALITY,   // == !=
+  PREC_COMPARISON, // < > <= >=
+  PREC_TERM,       // + -
+  PREC_FACTOR,     // * /
+  PREC_UNARY,      // ! -
+  PREC_CALL,       // . ()
+  PREC_PRIMARY
+} Precedence;
+
 typedef struct {
   Token previous;
   Token current;
